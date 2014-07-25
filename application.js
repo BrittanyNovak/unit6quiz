460073093c8e3618dfee428d44d40ac1d044b9fc
BrittanyNovak <brittanynicole0106@gmail.com>
2014-07-24 21:54:39 -0500

    added variables
    learned how to clear out text

--- application.js
+++ application.js
@@ -1,3 +1,8 @@
 function showTextBoxes(){
-	alert("you pressed the button");
+	var box1 = $("#one").val();
+	var box2 = $("#two").val();
+	var box3 = $("#three").val();
+	alert(box1 + box2 + box3);
+	$(".odd").val("");
+	$(".even").val("");
 } 
